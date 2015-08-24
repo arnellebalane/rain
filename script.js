11a89747a0515d7ff86e3bd3c453010bb30eb4c7
Japheth Balane <japheth162@gmail.com>
2015-08-25 06:17:35 +0800

    Light effects initial progress

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ canvas.height = window.innerHeight;
 
 var drops = [];
 
-setInterval(drawRain, 30);
+setInterval(drawWorld, 30);
 
 generateDrops(100);
 
@@ -16,8 +16,9 @@ function generateDrops(rainCount) {
 	};
 }
 
-function drawRain() {
+function drawWorld() {
 	clearCanvas();
+	drawLight();
 	for (var i = 0; i < drops.length; i++) {
 		drops[i].update().draw();
 	};
@@ -61,4 +62,56 @@ function Drop() {
 
 		return this;
 	}
+}
+
+function drawLight() {
+	context.beginPath();
+	context.moveTo(0, 0);
+	context.lineTo(1150, 0);
+	context.lineTo(1200, 50);
+	context.lineTo(1300, 300);
+	context.lineTo(1250, 500);
+	context.lineTo(1000, 700);
+	context.lineTo(700, canvas.height);
+	context.lineTo(200, canvas.height);
+	context.lineTo(0, canvas.height - 100);
+	context.fillStyle = "#0f0f00";
+	context.strokeStyle = "#0f0f00";
+	context.closePath();
+	context.fill();
+	context.stroke();	
+
+	context.beginPath();
+	context.moveTo(0, 50);
+	context.lineTo(75, 0);
+	context.lineTo(950, -50);
+	context.lineTo(1050, 50);
+	context.lineTo(1150, 250);
+	context.lineTo(1100, 350);
+	context.lineTo(950, 550);
+	context.lineTo(600, canvas.height - 100);
+	context.lineTo(150, canvas.height - 150);
+	context.lineTo(-50, canvas.height - 250);
+	context.fillStyle = "#131300";
+	context.strokeStyle = "#131300";
+	context.closePath();
+	context.fill();
+	context.stroke();
+
+	context.beginPath();
+	context.moveTo(500, 0);
+	context.lineTo(750, 0);
+	context.lineTo(850, 50);
+	context.lineTo(900, 100);
+	context.lineTo(875, 250);
+	context.lineTo(800, 350);
+	context.lineTo(600, 450);
+	context.lineTo(400, 500);
+	context.lineTo(100, 450);
+	context.lineTo(0, 350);
+	context.fillStyle = "#171700";
+	context.strokeStyle = "#171700";
+	context.closePath();
+	context.fill();
+	context.stroke();	
 }
